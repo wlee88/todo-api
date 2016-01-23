20c63f4f9660c1a2a156065ff8418e8ef20c84f4
William Lee <me@william-lee.com>
2016-01-23 22:17:52 +0000

    add todos api GET

--- server.js
+++ server.js
@@ -1,10 +1,36 @@
 var express = require('express');
 var app = express();
-var PORT = process.env.PORT || 3000
+var PORT = process.env.PORT || 3000;
+var todos = require('./todos.js');
 
 app.get('/', (req,res) => {
   res.send("TODO api Root");
-})
+});
+
+//GET /todos
+app.get('/todos', (req,res) => {
+  res.json(todos);
+});
+
+//GET /todo/:id
+
+app.get('/todos/:id', (req,res) => {
+  var todoId = parseInt(req.params.id, 10);
+  var matchedTodo;
+  todos.forEach((todo, index) => {
+    if (todo.id === todoId){
+      matchedTodo = todo;
+    };
+  });
+
+  if (matchedTodo) {
+    res.json(matchedTodo);
+  }
+  else {
+    res.status(404).send(`No Matches found for id: ${todoId}`);
+  }
+
+});
 
 app.listen(PORT, () => {
   console.log(`express is listening on port ${PORT}`);
---
+++ todos.js
@@ -0,0 +1,13 @@
+module.exports = [{
+  id: 1,
+  description: "Meet Danielle for lunch",
+  completed: false
+}, {
+  id:2,
+  description: "Go to Australia",
+  completed: false
+}, {
+  id:3,
+  description: "Buy BTBAM tickets",
+  completed: true,
+}];
